669df795e44e14d714021b8e6bf4b9196b8cc80e
Roma2308 <ramzanabduladyev@gmail.com>
2025-10-13 17:54:25 +0600

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/Header/Header"
+import HomePage from "./pages/HomePage"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <HomePage />
+    </div>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
 
 import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { FaUser } from "react-icons/fa";
+import "./Header.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className="menu flex gap-[40px]"> 
+        <a href="">New Drops 🔥</a>
+        <a href="">Men </a>
+        <a href="">Women </a>
+         </div>
+      <div className="logo"> 
+        <img className='w-[128px]' src="/public/logo.png" alt="" />
+         </div>
+      <div className="btns flex items-center gap-[42px]"> 
+        <span><CiSearch /></span>
+        <span><FaUser /></span>
+        <span className='span'> 0 </span>
+         </div>
+    </div>
   )
 }
 
