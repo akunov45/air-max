f6ff7901273d51f74e7cec4415c03c0d5ec81cdc
Baratali07 <timabek6@gmail.com>
2025-10-13 17:54:10 +0600

    header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,27 @@
 
 import React from 'react'
+import { IoSearch } from "react-icons/io5";
+import { FaUser } from "react-icons/fa6";
+import "./Header.css"
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header app-container'>
+      <div className='menu flex gap-[40px] '>
+        <a href="">New Drops 🔥</a>
+        <a href="">Men</a>
+        <a href="">Woman</a>
+      </div>
+      <div className='logo'>
+        <img className='w-[128px]' src="/public/logo.png" alt="" />
+      </div>
+      <div className='btns flex items-center gap-[42px]'>
+        <span><IoSearch/></span>
+        <span><FaUser/></span>
+        <span className='span'> 0 </span>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import HeroImg from '/hero.png'
+
+const Hero = () => {
+  return (
+    <div style={{
+      backgroundImage: `url(${HeroImg})`,
+      height: "750px"
+    }}>
+      <div className={`bg-cover bg-no-repeat`}>
+        <h2>NIKE AIR MAX</h2>
+        <p>Nike introducing the new air max for everyone's comfort</p>
+        <button>Shop now</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
