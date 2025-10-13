e4a9be96d81f5ea658cbd9c72b9a43f8eddbb3e5
Akunov4594 <86850933+akunov45@users.noreply.github.com>
2025-10-13 17:54:24 +0600

    header done

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+
+import HeroImg from "/hero.png"
+
+const Hero = () => {
+  return (
+    <div style={{
+      backgroundImage: `url(${HeroImg})`,
+      height: "750px"
+    }}
+      className={`bg-cover bg-no-repeat`}>
+      <h2>NIKE AIR MAX</h2>
+      <p>Nike introducing the new air max for everyone's comfort</p>
+      <button>Shop now</button>
+    </div>
+  )
+}
+
+export default Hero
