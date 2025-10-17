a577b5b6dd4dc30fe91ea22bf11ce38a04ecb5c5
Akunov4594 <86850933+akunov45@users.noreply.github.com>
2025-10-17 17:39:04 +0600

    add supabase 3

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,33 +1,15 @@
-
-
-<<<<<<< HEAD
 import Button from '../UI/Button'
 import './ProductList.css'
-=======
-
-import React from 'react'
-import "./ProductList.css"  
-import Button from '../UI/Button'
->>>>>>> 3fd14e0afa1b427d401f569f2dac137373d14952
 
 const ProductList = () => {
   return (
     <div>
-<<<<<<< HEAD
       <div className='product-title'>
         <h2>Don’t miss out new drops</h2>
         <Button>
           Shop New Drops
         </Button>
       </div>
-=======
-        <div className="product-title">
-        <h2>Don’t miss out new drops</h2>
-        <Button>
-            Shop new drops
-        </Button>
-        </div>
->>>>>>> 3fd14e0afa1b427d401f569f2dac137373d14952
     </div>
   )
 }
