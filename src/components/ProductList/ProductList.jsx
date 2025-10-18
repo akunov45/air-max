c2dd5aeb9c171ec8975b2898879b9dcd81b21226
UMAR099 <u263823@gmail.com>
2025-10-18 14:08:33 +0600

    ess

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,6 +2,7 @@ import Button from '../UI/Button'
 import './ProductList.css'
 
 const ProductList = () => {
+
   return (
     <div>
       <div className='product-title'>
@@ -9,6 +10,18 @@ const ProductList = () => {
         <Button>
           Shop New Drops
         </Button>
+      </div>
+      <div className='products'>
+      <div className='product'>
+        <div className='img'>
+          <img src="https://www.tradesports.co.uk/cdn/shop/files/GY8424_01.jpg?v=1729251462" alt="" />
+          <span>New</span>
+        </div>
+        <div className='info'>
+          <h4>ADIDAS 4DFWD X PARLEY RUNNING SHOES</h4>
+          <button>View Product - <span>$125</span></button>
+        </div>
+      </div>
       </div>
     </div>
   )
