c555c8fb587cf7c5940e1da03cbbaedb5e635a70
UMAR099 <u263823@gmail.com>
2025-10-20 17:50:01 +0600

    'umar'

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,16 +31,16 @@ if(products.length === 0){
         <Button>Shop New Drops</Button>
       </div>
 
-      <div className="drops flex items-center justify-center gap-[32px] flex-wrap">
+      <div className="drops flex items-center justify-center gap-[16px] flex-wrap">
         {allProducts?.length > 0 ? (
           allProducts.map((item) => (
-            <div className="drop" key={item.id}>
+            <div className="drop rounded-[28px]" key={item.id}>
               <div className="img">
                 <img src={item.images[0] || '/public/jordan.png'} alt={item.name} />
                 {item.is_new && <span>New</span>}
               </div>
               <div className="title">
-                <h4>{item.name}</h4>
+                <h4 className='line-clamp-1'>{item.name}</h4>
                 <button>
                   View product - <span>${item.price}</span>
                 </button>
