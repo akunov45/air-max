af34c04daba0d38259c05675efc045c003b89672
Akunov4594 <86850933+akunov45@users.noreply.github.com>
2025-10-20 17:40:24 +0600

    lesson 10 end

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -10,6 +10,18 @@ const ProductList = () => {
           Shop New Drops
         </Button>
       </div>
+      <div className='products'>
+        <div className='product'>
+          <div className='img'>
+            <img src="https://www.tradesports.co.uk/cdn/shop/files/GY8424_01.jpg?v=1729251462" alt="" />
+            <span>New</span>
+          </div>
+          <div className='info'>
+            <h4>ADIDAS 4DFWD X PARLEY RUNNING SHOES</h4>
+            <button>View Product - <span>$125</span></button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
